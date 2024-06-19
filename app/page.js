b76c006b21e32330c6d6eebdcb191093385571a7
Harshal Tupe <harshaltupe12@gmail.com>
2024-06-19 09:31:09 +0530

    initial configuration

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="">
+      <h2>Hello world from harshal</h2>
+      <Button> Test me</Button>
+    </div>
+  );
+}

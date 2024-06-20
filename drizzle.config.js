b791878aa99db77ab90e5614e6377553b4bdd528
Harshal Tupe <harshaltupe12@gmail.com>
2024-06-20 20:16:43 +0530

    Drizzle configuration done...

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,8 +1,8 @@
 /** @type { import("drizzle-kit").Config } */
 export default {
-    schema: "./schema.ts",
+    schema: "./utils/schema.js",
     dialect: 'postgresql',
     dbCredentials: {
-      url: "postgresql://mock%20master_owner:vP4LlnBFUdX8@ep-patient-butterfly-a187sbtr.ap-southeast-1.aws.neon.tech/mock%20master?sslmode=require",
+      url: "postgresql://mock%20master_owner:vP4LlnBFUdX8@ep-patient-butterfly-a187sbtr.ap-southeast-1.aws.neon.tech/mock-master?sslmode=require",
     }
   };

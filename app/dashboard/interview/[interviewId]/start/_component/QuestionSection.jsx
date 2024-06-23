788fe9e98b8b31fac4d13ff6cdc6cfcf8f3e9a1d
Harshal Tupe <harshaltupe12@gmail.com>
2024-06-23 22:45:59 +0530

    created 2 components

--- app/dashboard/interview/[interviewId]/start/_component/QuestionSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_component/QuestionSection.jsx
@@ -1,7 +1,16 @@
-import { Lightbulb } from "lucide-react";
+import { Lightbulb, Volume2 } from "lucide-react";
 import React from "react";
 
 function QuestionSection({ mockInterviewQuestion, activeQuestionIndex }) {
+  const textToSpeech = (text) => {
+    if('speechSynthesis' in window){
+      const speech = new SpeechSynthesisUtterance(text);
+      window.speechSynthesis.speak(speech)
+    }
+    else{
+      alert("Sorry your browser does not support text-to-speech")
+    }
+  };
   return (
     <div className="p-5 border rounded-lg my-10">
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
@@ -16,13 +25,22 @@ function QuestionSection({ mockInterviewQuestion, activeQuestionIndex }) {
             </h2>
           ))}
       </div>
-      <h2 className="my-5 text-sm md:text-lg">{mockInterviewQuestion[activeQuestionIndex]?.question}</h2>
+      <h2 className="my-5 text-sm md:text-lg">
+        {mockInterviewQuestion[activeQuestionIndex]?.question}
+      </h2>
+      <Volume2 className="cursor-pointer"
+        onClick={() =>
+          textToSpeech(mockInterviewQuestion[activeQuestionIndex]?.question)
+        }
+      />
       <div className="border border-blue-400 rounded-lg p-5 bg-blue-100 mt-20">
         <h2 className="flex gap-2 items-center text-primary">
-            <Lightbulb/>
-            <strong>Note :</strong>
-        </h2>   
-        <h2 className="text-sm text-primary my-2">{process.env.NEXT_PUBLIC_QUESTION_NOTE}</h2>
+          <Lightbulb />
+          <strong>Note :</strong>
+        </h2>
+        <h2 className="text-sm text-primary my-2">
+          {process.env.NEXT_PUBLIC_QUESTION_NOTE}
+        </h2>
       </div>
     </div>
   );
